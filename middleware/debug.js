71247af43399c0fa01579add6676168ee36b14d4
Henrik Joreteg <hjoreteg@gmail.com>
2016-08-30 21:46:11 -0700

    added thing

--- middleware/debug.js
+++ middleware/debug.js
@@ -11,6 +11,7 @@ module.exports = store => next => action => {
 
   if (debug) {
     console.debug('state:', store.getState())
+    window.listSelectors && window.listSelectors()
     window.listActiveEffects && window.listActiveEffects()
     console.groupEnd(action.type)
   }
