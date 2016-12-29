0ac600b3c0dae9b1564a299356fc63af6ae0ae11
Henrik Joreteg <hjoreteg@gmail.com>
2016-12-29 10:51:00 -0800

    make sure 'dispatch' is available

--- src/middleware/custom-thunk.js
+++ src/middleware/custom-thunk.js
@@ -1,7 +1,7 @@
 function createThunkMiddleware (extra) {
   return ({ dispatch, getState, store }) => next => action => {
     if (typeof action === 'function') {
-      return action(Object.assign({}, store, extra))
+      return action(Object.assign({}, store, extra, {dispatch}))
     }
     return next(action)
   }
