74813e920fedc17374a5f725ac488180d1050f36
Oli Evans <oli@tableflip.io>
2018-05-10 11:16:33 +0100

    Parameterise the selector for create-route-bundle
    By letting the user pass in the selector to use to get the routeInfo
    they override the default with selectHash, which allows hash based
    routing in their app.

--- src/bundles/create-route-bundle.js
+++ src/bundles/create-route-bundle.js
@@ -1,9 +1,9 @@
 import { createSelector } from 'create-selector'
 import createRouteMatcher from 'feather-route-matcher'
 
-export default routes => ({
+export default (routes, routeInfoSelector = 'selectPathname') => ({
   name: 'routes',
-  selectRouteInfo: createSelector('selectPathname', createRouteMatcher(routes)),
+  selectRouteInfo: createSelector(routeInfoSelector, createRouteMatcher(routes)),
   selectRouteParams: createSelector('selectRouteInfo', ({ params }) => params),
   selectRoute: createSelector('selectRouteInfo', ({ page }) => page)
 })
