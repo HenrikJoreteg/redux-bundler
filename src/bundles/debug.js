408f88417b3205b18b39f30f2649420c4e3e16f3
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-06-29 10:49:47 -0500

    move logging into their own functions

--- src/bundles/debug.js
+++ src/bundles/debug.js
@@ -4,6 +4,10 @@ import debugMiddleware from '../middleware/debug'
 
 const ENABLE = 'DEBUG_ENABLED'
 const DISABLE = 'DEBUG_DISABLED'
+const colorTitle = 'color: #1676D2;'
+const black = 'color: black;'
+const colorGreen = 'color: #4CAF50;'
+const colorOrange = 'color: #F57C00;'
 
 export default {
   name: 'debug',
@@ -18,71 +22,67 @@ export default {
   },
   doEnableDebug: () => ({ type: ENABLE }),
   doDisableDebug: () => ({ type: DISABLE }),
+  doLogBundles: () => ({ store }) => {
+    const names = store.meta.chunks[0].bundleNames
+    return console.log(
+      '%cinstalled bundles:\n  %c%s',
+      colorTitle,
+      black,
+      names.join('\n  ')
+    )
+  },
+  doLogSelectors: () => ({ store }) => {
+    if (!store.selectAll) return console.log('NOPE')
+    console.log('%cselectors:', colorGreen, store.selectAll())
+  },
+  doLogReactors: () => ({ store }) => {
+    console.groupCollapsed('%creactors', colorOrange)
+    const { reactorNames } = store.meta
+    reactorNames.forEach(name => console.log(name))
+    console.groupEnd()
+  },
+  doLogActionCreators: () => ({ store }) => {
+    const actionCreators = []
+    for (const key in store) {
+      if (key.indexOf('do') === 0) {
+        actionCreators.push(key)
+      }
+    }
+    actionCreators.sort()
+    console.groupCollapsed('%caction creators', colorOrange)
+    actionCreators.forEach(name => console.log(name))
+    console.groupEnd()
+  },
+  doLogNextReaction: () => ({ store }) => {
+    {
+      const { nextReaction, activeReactor } = store
+      if (nextReaction) {
+        console.log(
+          `%cnext reaction:\n  %c${activeReactor}`,
+          colorOrange,
+          black,
+          nextReaction
+        )
+      }
+    }
+  },
+  doLogEverything: () => ({ store }) => {
+    console.groupCollapsed('%credux bundler', colorTitle)
+    store.doLogBundles()
+    store.doLogSelectors()
+    store.doLogReactors()
+    store.doLogActionCreators()
+    store.doLogNextReaction()
+    console.groupEnd()
+    if (store.isReacting) {
+      console.log(`%cqueuing reaction:`, colorOrange)
+    }
+  },
   selectIsDebug: state => state.debug,
   getMiddleware: () => debugMiddleware,
   init: store => {
     if (store.selectIsDebug()) {
-      const names = store.meta.chunks[0].bundleNames
-      self.store = store
-      const actionCreators = []
-      for (const key in store) {
-        if (key.indexOf('do') === 0) {
-          actionCreators.push(key)
-        }
-      }
-      actionCreators.sort()
-      const colorTitle = 'color: #1676D2;'
-      const black = 'color: black;'
-      const colorGreen = 'color: #4CAF50;'
-      const colorOrange = 'color: #F57C00;'
-
-      store.logSelectors = self.logSelectors = () => {
-        if (!store.selectAll) return
-        console.log('%cselectors:', colorGreen, store.selectAll())
-      }
-
-      store.logBundles = self.logBundles = () => {
-        console.log(
-          '%cinstalled bundles:\n  %c%s',
-          colorTitle,
-          black,
-          names.join('\n  ')
-        )
-      }
-
-      store.logActionCreators = self.logActionCreators = () => {
-        console.groupCollapsed('%caction creators', colorOrange)
-        actionCreators.forEach(name => console.log(name))
-        console.groupEnd()
-      }
-
-      store.logReactors = self.logReactors = () => {
-        console.groupCollapsed('%creactors', colorOrange)
-        const { reactorNames } = store.meta
-        reactorNames.forEach(name => console.log(name))
-        console.groupEnd()
-      }
-
-      store.logNextReaction = self.logNextReaction = () => {
-        const { nextReaction, activeReactor } = store
-        if (nextReaction) {
-          console.log(
-            `%cnext reaction:\n  %c${activeReactor}`,
-            colorOrange,
-            black,
-            nextReaction
-          )
-        }
-      }
-
-      console.groupCollapsed('%credux bundler', colorTitle)
-      store.logBundles()
-      store.logSelectors()
-      store.logReactors()
-      console.groupEnd()
-      if (store.isReacting) {
-        console.log(`%cqueuing reaction:`, colorOrange)
-      }
+      this.doLogEverything()
     }
   }
 }
