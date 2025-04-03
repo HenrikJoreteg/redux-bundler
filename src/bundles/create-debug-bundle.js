766d2ea6f0f5cbccf1e2d24f4322c2b196a5e5b3
Philip Roberts <phil@xchart.com>
2025-04-03 09:32:06 -0400

    Remove log

--- src/bundles/create-debug-bundle.js
+++ src/bundles/create-debug-bundle.js
@@ -50,7 +50,6 @@ export default spec => {
           opts.logStackTraces &&
           Error.stackTraceLimit === initialStackTraceLimit
         ) {
-          console.log('ENABLE')
           Error.stackTraceLimit = opts.stackTraceLimit
         }
 
